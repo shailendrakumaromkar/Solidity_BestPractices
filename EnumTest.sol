16cdbb96772ccadda86784bf718d12d168c9549e
om <ShailendraKumar.Omkar@softwareag.com>
2020-09-18 12:13:33 +0530

    updated enum

--- EnumTest.sol
+++ EnumTest.sol
@@ -3,9 +3,19 @@ pragma solidity 0.6.0;
 contract EnumTest {
 
     enum Direction {North, South, East,West}
+    Direction dir;
+    Direction constant defaultDirection=Direction.West;
 
-    function setDirection() public pure returns (Direction){
-        return Direction.North;
+    function setDirection() public {
+        dir= Direction.East;
         
+    }
+    
+    function getDirection() public view returns (Direction) {
+        return dir;
+    }
+    
+    function getDefaultDirection() public pure returns(Direction) {
+        return defaultDirection;
     }
 }
