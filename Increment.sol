af143a39b0d6ec1874bab381ad22fdb48a070606
SHAILENDRA OMKAR <shailendrakumaromkar@gmail.com>
2020-11-26 13:43:57 +0530

    Update Increment.sol

--- Increment.sol
+++ Increment.sol
@@ -7,7 +7,7 @@ contract Increment {
     function incrementCounter () public {
        counter=counter+1;
     }
-    // increment 
+    // increment counter
    function getIncrement() public view returns (uint) {
        return counter;
    }
