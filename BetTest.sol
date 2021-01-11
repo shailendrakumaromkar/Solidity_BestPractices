48fc654a9a9e839b24fc3ada63d96ddd9b22f6ca
shailendrakumaromkar <shailendrakumaromkar@gmail.com>
2021-01-12 03:48:55 +0530

    updated file

--- BetTest.sol
+++ BetTest.sol
@@ -7,7 +7,7 @@ contract div {
     uint256 public totalAmount;
     bool public isBetActive=false;
     
-    // contract constructor run once at contract deployment
+    // contract constructo run once at contract deployment
     constructor()  {
         // the manager is account address that deploys the contract
         manager = msg.sender; 
