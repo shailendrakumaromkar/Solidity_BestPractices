f11e2858c42f3fe6161ac755e3c82b70a9917c11
shailendrakumaromkar <shailendrakumaromkar@gmail.com>
2021-02-02 09:39:19 +0530

    updated

--- EnumTest.sol
+++ EnumTest.sol
@@ -6,7 +6,7 @@ contract EnumTest {
     Direction dir;
     Direction constant defaultDirection=Direction.West;
 
-//   functionssssssssssssssssssssssssssdssss
+//   functionssssssssssssssssssssssssssdsss
     function setDirection() public {
         dir= Direction.East;
         
