771a2a64a3a64574564aa45fd6abd7dc2d4e66ca
shailendrakumaromkar <shailendrakumaromkar@gmail.com>
2021-03-23 11:25:57 +0530

    new file added

--- Increment.sol
+++ Increment.sol
@@ -3,7 +3,7 @@ pragma solidity 0.6.0;
 
 contract Increment {
     uint counter;
-    //incrementsssssssssssssssssssssssss
+    //incrementssssssssssssssssssssssss
     function incrementCounter () public {
        counter=counter+1;
     }
