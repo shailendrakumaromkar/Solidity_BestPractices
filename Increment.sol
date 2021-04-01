2666995df01fa9df8c405145228a70e4d632fe88
shailendrakumaromkar <shailendrakumaromkar@gmail.com>
2021-04-01 10:26:14 +0530

    new file added

--- Increment.sol
+++ Increment.sol
@@ -7,7 +7,7 @@ contract Increment {
     function incrementCounter () public {
        counter=counter+1;
     }
-    // increment  functionssssssssssssssssssssssssss
+    // increment  functionsssssssssssssssssssssssss
    function getIncrement() public view returns (uint) {
        return counter;
    }
