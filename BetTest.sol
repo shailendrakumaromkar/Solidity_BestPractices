98ae09ff54dbd5d618b744d58f2cdb2b278b460b
SHAILENDRA OMKAR <shailendrakumaromkar@gmail.com>
2021-04-11 00:39:20 +0530

    Update BetTest.sol

--- BetTest.sol
+++ BetTest.sol
@@ -13,7 +13,7 @@ contract div {
         manager = msg.sender; 
     }
 
-    // this fallback payable function will be automatically called when somebodys
+    // this fallback payable function will be automatically called when somebody
     //sends ether to our contract addresss
     function betAmount() payable public {
         require(msg.value >= 0.01 ether);
