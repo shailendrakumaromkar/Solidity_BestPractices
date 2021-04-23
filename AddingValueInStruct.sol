29d796bd30c4baec6b6ed40f8a6892a07186e503
SHAILENDRA OMKAR <shailendrakumaromkar@gmail.com>
2021-04-23 17:09:47 +0530

    Update AddingValueInStruct.sol

--- AddingValueInStruct.sol
+++ AddingValueInStruct.sol
@@ -3,7 +3,7 @@ contract AttendanceRegister {
             string name;
             uint class;
         }
-//eventssssssssssssssssssssssssssssssssssssssssssssss
+//eventsssssssssssssssssssssssssssssssssssssssssssss
     event Added(string name, uint class, uint time);
 
     mapping(uint => Student) public register; // roll number => student details
